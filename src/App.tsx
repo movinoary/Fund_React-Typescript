5522963650a1d2e0d02b61614b151efd1ce2c3d0
vinoarystio <rocevo.id@gmail.com>
2022-07-19 22:47:52 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import ClassComp from "./1.classComponents";
+import FuncCom from "./2.functionComponents";
 
 function App() {
   return (
     <div className="App">
-      <ClassComp title="Class Components" namaHalaman="class" />
+      {/* <ClassComp title="Class Components" namaHalaman="class" /> */}
+      <FuncCom title="function Components">
+        <p> Child Props</p>
+      </FuncCom>
     </div>
   );
 }
