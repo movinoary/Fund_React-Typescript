4bd1a8c449a4d35359aa85bd96921453c78f3798
vinoarystio <rocevo.id@gmail.com>
2022-07-19 23:09:01 +0700

    hooks

--- src/3.constComponents/index.tsx
+++ src/3.constComponents/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren, useState } from "react";
+import React, { FC, PropsWithChildren, useEffect, useState } from "react";
 
 type Props = {
   title: string;
@@ -7,10 +7,34 @@ type Props = {
 // FC = FunctionComponent
 const ConstComp: FC<Props> = props => {
   const [counter, setCounter] = useState<number>(0);
+  const [user, setUser] = useState<{ fristName: string; lastName: string }>({
+    fristName: "",
+    lastName: "",
+  });
 
   function addCounter(param: number): void {
     setCounter(param + 1);
   }
+
+  function _login() {
+    setUser({
+      fristName: "vino",
+      lastName: "arystio",
+    });
+  }
+
+  function _logout() {
+    setUser({
+      fristName: "",
+      lastName: "",
+    });
+  }
+
+  useEffect(() => {
+    // get data dari server
+    console.log("get Data");
+  }, [JSON.stringify(user)]);
+
   return (
     <div>
       <h1>{props.title}</h1>
@@ -21,8 +45,25 @@ const ConstComp: FC<Props> = props => {
         {/* Bisa diIsi null atau nama function */}
         <button onClick={addCounter.bind(null, counter)}>add counter</button>
       </div>
+      <div>
+        <h1>{user.fristName}</h1>
+        <h2>{user.lastName}</h2>
+        {user.fristName !== "" && <DataUser />}
+        <button onClick={_login}>Login</button>
+        <button onClick={_logout}>Logout</button>
+      </div>
     </div>
   );
+};
+
+const DataUser: FC = () => {
+  useEffect(() => {
+    return () => {
+      console.log("Component Hilang");
+    };
+  }, []);
+
+  return <div>Berhasil Login</div>;
 };
 
 export default ConstComp;
